dda0390304cf411fcc45ab92b0b4513bbfefec57
Edward Osorno <edward-1094@hotmail.com>
2019-10-28 23:24:33 +0000

    modify _printf while to find %

--- _printf.c
+++ _printf.c
@@ -6,20 +6,25 @@ int _printf(const char *format, ...)
 {
     int i = 0, byte = 0;
     int (*execute)(va_list ap);
-
     va_list ap;
+
     va_start(ap, format);
 
     while (format[i])
     {
-        if (!format[i])
-            return(-1);
+       if (format[i] == '%')
+       {
+	       execute = get_ops_function(format[i + 1]);
+	       byte = byte + execute(ap);
+	       i++;
+       }
+       else
+       {
+	       write(1, &format[i], 1);
+	       byte = byte + 1;
+       }
 
-        if (format[i] == '%')
-            execute = get_ops_function(format[i + 1]);
-
-        byte = execute(ap);
-        i++;
+       i++;
     }
     va_end(ap);
     return(byte);
