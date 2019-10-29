73b7f3fe93d68f5f07b1bfed052adcc0358e4f0c
Edward Osorno <edward-1094@hotmail.com>
2019-10-29 00:38:36 +0000

    add print str

--- holberton.h
+++ holberton.h
@@ -6,7 +6,7 @@ int _putchar(int c);
 int _printf(const char *format, ...);
 int (*get_ops_function(char s))(va_list ap);
 int prt_chr(va_list ap);
-int ptr_str(va_list ap);
+int prt_str(va_list ap);
 
 
 typedef struct data
